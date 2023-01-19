dc9923cf37cf90bb7c448b4008de5e62b141f7ad
shunsuke shimada <b2201240@photon.chitose.ac.jp>
2023-01-19 11:57:51 +0900

    計算結果に代入先を変更

--- yubimoji/Similaritycalculation.py
+++ yubimoji/Similaritycalculation.py
@@ -26,21 +26,17 @@ df_20_0 = df_fing_20 - df_fing_0
 
 v_x2 = np.array([])
 
-for i in range(2, 35, 16):
-    v_x2 = np.append(v_x2, df_4_0.iat[0, i])
+for j in range(2, 35, 16):
+    v_x1 = np.append(v_x1, df2.iat[0, j])
 
-for i in range(2, 35, 16):
-    v_x2 = np.append(v_x2, df_8_0.iat[0, i])
-
-for i in range(2, 35, 16):
-    v_x2 = np.append(v_x2, df_12_0.iat[0, i])
-
-for i in range(2, 35, 16):
-    v_x2 = np.append(v_x2, df_16_0.iat[0, i])
-
-for i in range(2, 35, 16):
-    v_x2 = np.append(v_x2, df_20_0.iat[0, i])
+    v_x2 = np.append(v_x2, df_4_0.iat[0, j])
+    v_x2 = np.append(v_x2, df_8_0.iat[0, j])
+    v_x2 = np.append(v_x2, df_12_0.iat[0, j])
+    v_x2 = np.append(v_x2, df_16_0.iat[0, j])
+    v_x2 = np.append(v_x2, df_20_0.iat[0, j])
 
 v = []
 v.append(v_x1)
 v.append(v_x2)
+V = []
+V = v
